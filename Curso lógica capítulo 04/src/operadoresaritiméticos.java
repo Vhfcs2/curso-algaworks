e98986aef630e67623e6b520cc757b3526c77d88
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-11-26 17:06:42 -0800

    capítulo 04

---
+++ Curso lógica capítulo 04/src/operadoresaritiméticos.java
@@ -0,0 +1,46 @@
+
+public class operadoresaritiméticos {
+
+	public static void main(String[] args) {
+		/* Integer adicao = 1 + 1;
+		System.out.println("Adição: " + adicao);
+		
+		Integer subtracao = 1 - 1;
+		System.out.println("Subtração: " + subtracao);
+		
+		Integer multiplicacao = 2 * 1;
+		System.out.println("Multiplicação: " + multiplicacao);
+		
+		Integer divisao = 4 / 2;
+		System.out.println("Divisão: " + divisao);
+		
+		Integer precedencia01 = 2 + 10 * 70;
+		System.out.println("Precedencia 01: " + precedencia01);
+		
+		Integer precedencia02 = (2 + 10) * 70;
+		System.out.println("Precedencia 02: " + precedencia02); 
+		
+		Integer precedencia03 = 70 * (2 + 10);
+		System.out.println("Precedencia 03: " + precedencia03); 
+		
+		Integer precedencia04 = 70 * (2 + 10) + 2;
+		System.out.println("Precedencia 04: " + precedencia04);  */
+	    
+		//-----------------------------------------------------------------------------------------------------
+		
+		//exercício
+		
+		Integer calculadora01 = (10 + 77) * 50 / 2;
+				System.out.println("Calculadora: " + calculadora01);
+				
+		Integer calculadora02 = 10 + 77 * 50 / 2;
+				System.out.println("Calculadora: " + calculadora02);
+				
+		Integer calculadora03 = 30 + 7 * 600 / 8;
+				System.out.println("Calculadora: " + calculadora03);
+	
+	
+	
+	}
+	
+}
---
+++ Curso lógica capítulo 04/src/operadoresrelacionais.java
@@ -0,0 +1,21 @@
+
+public class operadoresrelacionais {
+
+	public static void main(String[] args) {
+		Boolean tresMaiorQueDois = 3 > 2;
+		System.out.println("3 > 2? " + tresMaiorQueDois);
+		
+		Boolean tresMenorQueDois = 3 < 2;
+		System.out.println("3 < 2? " + tresMenorQueDois);
+		
+		Boolean tresMaiorOuIgualATres = 3 >= 3;
+		System.out.println("3 >= 3? " + tresMaiorOuIgualATres);
+		
+		Boolean tresMenorOuIgualATres = 3 >= 3;
+		System.out.println("3 >= 3? " + tresMenorOuIgualATres);
+	
+		
+	} 
+	
+}
+ 
