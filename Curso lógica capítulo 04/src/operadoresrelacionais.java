a06b40ba902d7ff2daf6ac71e468f964b79c44da
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-11-27 12:33:16 -0800

    capítulo -4: operadores relacionais

--- Curso lógica capítulo 04/src/operadoresrelacionais.java
+++ Curso lógica capítulo 04/src/operadoresrelacionais.java
@@ -2,6 +2,11 @@
 public class operadoresrelacionais {
 
 	public static void main(String[] args) {
+		//==: igual
+		//&&: "e"
+		//||: "ou"
+		//!=: "diferente"
+		
 		Boolean tresMaiorQueDois = 3 > 2;
 		System.out.println("3 > 2? " + tresMaiorQueDois);
 		
@@ -11,8 +16,19 @@ public class operadoresrelacionais {
 		Boolean tresMaiorOuIgualATres = 3 >= 3;
 		System.out.println("3 >= 3? " + tresMaiorOuIgualATres);
 		
-		Boolean tresMenorOuIgualATres = 3 >= 3;
-		System.out.println("3 >= 3? " + tresMenorOuIgualATres);
+		Boolean tresMenorOuIgualATres = 3 <= 3;
+		System.out.println("3 <= 3? " + tresMenorOuIgualATres);
+		
+		Boolean doisIgualADois = 2 == 2;
+		System.out.println("2 = 2? " + doisIgualADois );
+		
+		Boolean doisDiferenteDeADois = 2 != 2;
+		System.out.println("2 != 2? " + doisDiferenteDeADois);
+		
+		Integer quatro = 4;
+		Boolean quatroMaiorQueQuatro = quatro > quatro;
+		System.out.println("quatro > quatro? " + quatroMaiorQueQuatro);
+		
 	
 		
 	} 
