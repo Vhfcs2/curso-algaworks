2c341b7de8581ba05e2bf57873265213b8ca66b9
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-11-30 10:38:59 -0800

    capítulo 4 (concluído)

---
+++ Curso lógica capítulo 04/src/ConcatenacaoTextos.java
@@ -0,0 +1,14 @@
+
+public class ConcatenacaoTextos {
+
+	public static void main(String[] args) {
+		String inicio = "isto ";
+		String meio = "é apenas ";
+		String fim = "um exemplo ";
+		
+		System.out.println(inicio + meio + fim);
+		
+		System.out.println("Esse " + meio + "um exmeplo de concatenação de textos");
+	}
+	
+}
---
+++ Curso lógica capítulo 04/src/OperadoresIncrementoDecremento.java
@@ -0,0 +1,17 @@
+
+public class OperadoresIncrementoDecremento {
+
+	public static void main(String[] args) {
+		//Integer numero =10;
+		//System.out.println("Número antes:" + numero);
+
+		//++numero;
+		//System.out.println("Número depois:" + numero);
+		
+		Integer numero01 = 10;
+		Integer numero02 = ++numero01;
+		
+		System.out.println("Número01: " + numero01 + ", " +  "Número02: " + numero02) ;
+	}
+	
+}
