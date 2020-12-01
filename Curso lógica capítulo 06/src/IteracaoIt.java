66c94a84fdb432b5c4e38e818425ba75feeb6c17
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-12-01 14:19:25 -0800

    capítulo 06 corrigido (FINAL)

---
+++ Curso lógica capítulo 06/src/IteracaoIt.java
@@ -0,0 +1,38 @@
+
+public class IteracaoIt {
+
+	public static void main(String[] args) {
+		
+		IteracaoIt it = new IteracaoIt();
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("Kageyama Baka.");
+//		}
+
+//		int i = 0;
+//		while (i < 10) {
+//			if (i == 5) {
+//				System.out.println("vai parar!");
+//				break;
+//				
+//			}
+//			System.out.println("Kageyama Baka.");
+//			i++;
+//		}		
+		int i = 0;
+		while (i < 10) {
+			if (i == 5) {
+				System.out.println("vai continuar...");
+				i++;
+				continue;
+
+			}
+
+			System.out.println(i + ": Kageyama Baka.");
+			i++;
+
+		}
+
+	}
+
+
+}
