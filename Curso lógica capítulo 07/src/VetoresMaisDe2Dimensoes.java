9b7273dbc5dc8a1d24a2bca73555e903f2d2bd45
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-12-03 13:01:49 -0800

    capítulo 07

---
+++ Curso lógica capítulo 07/src/VetoresMaisDe2Dimensoes.java
@@ -0,0 +1,27 @@
+
+public class VetoresMaisDe2Dimensoes {
+
+	public static void main(String[] args) {
+		Double faturamentoDoDia = 1000.0;
+		
+		Double[] faturamentoJaneiro = new Double[] { 1500.0, 2000.0, 1700.0 };
+		Double[] faturamentoFevereiro = new Double[] { 1400.0, 2600.0, 1800.0 };
+		
+		Double[][] faturamentoAnual = new Double[][] { faturamentoJaneiro, faturamentoFevereiro };
+		
+//		System.out.println("Faturamento do dia 1º de Janeiro: " + faturamentoAnual[0] [0]);
+//		System.out.println("Faturamento do dia 3 de Janeiro: " + faturamentoAnual[1] [2]);
+		
+		for (int i = 0; i <faturamentoAnual.length; i++) {
+			System.out.println("Mês: " + (i + 1));
+			
+			Double[] mes = faturamentoAnual[i];
+			
+			for(int y = 0; y < mes.length; y++) {
+				Double dia = mes[y];
+				System.out.println("Dia " + (y +1) + ": " + dia);
+			}
+		}
+	}
+
+}
