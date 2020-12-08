1dc8550a3769aaaccb6e8befd6caf5b0718be3e5
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-12-07 16:53:01 -0800

    capítulo 09

---
+++ Curso lógica capítulo 09/src/primeiraclasse/exercicio01Parte2.java
@@ -0,0 +1,23 @@
+package primeiraclasse;
+
+public class exercicio01Parte2 {
+
+	public static void main(String[] args) {
+
+		Produto produto = new Produto();
+		produto.nome = "PS4";
+		produto.quantidadeEstoque = 8;
+
+		System.out.println(
+				"Necessário repor estoque do produto " + produto.nome + "? " + eNecessarioReporEstoque(produto));
+	}
+
+	static Boolean eNecessarioReporEstoque(Produto produto) {
+		if (produto.quantidadeEstoque < Produto.QUANTIDADE_MINIMA_ESTOQUE) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
---
+++ Curso lógica capítulo 09/src/primeiraclasse/exercicio01Produto.java
@@ -0,0 +1,11 @@
+package primeiraclasse;
+
+public class exercicio01Produto {
+	
+static final Integer QUANTIDADE_MINIMA_ESTOQUE = 10;
+	
+	String nome;
+	
+	Integer quantidadeEstoque;
+
+}
